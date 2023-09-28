6a789de60de38519cd88cfb83deee66162818a45
jessica sharp <jsharp.prsvr@gmail.com>
2023-09-28 09:58:08 -0500

    completed

---
+++ main.js
@@ -0,0 +1,41 @@
+/******************************
+ * YOUR CODE GOES RIGHT HERE! *
+ ******************************/
+function fizzy(num){
+  if (num  %3 === 0 && num %5 === 0){
+    return 'FizzBuzz'
+  }else if(num %3 === 0){
+    return 'Fizz'
+  
+  }else if(num %5 === 0){
+    return 'Buzz'
+  }else{
+    return num
+  }
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+/**********************************
+ *      OUR CODE GOES BELOW.      *
+ * THIS IS FOR INTERNAL USE ONLY. *
+ * DO NOT ADD OR CHANGE ANYTHING! *
+ **********************************/
+
+
+if (typeof fizzy === "undefined") {
+  fizzy = undefined;
+}
+
+module.exports = {
+  fizzy,
+};
